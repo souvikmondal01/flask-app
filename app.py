d09a23fa37366f0a07f05ca566321ef08dea9581
Souvik Mondal <souvikmondal2120@gmail.com>
2023-01-28 17:13:48 +0530

    6

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,6 +13,18 @@ def index():
 @app.route("/hello", methods=["GET"])
 def hello():
     return jsonify(msg="hello world")
+
+
+@app.route("/signup", methods=["POST","GET"])
+def signup():
+    name = request.form["name"]
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
+
+    user_dict = {"name": name, "username": username, "email": email}
+
+    return user_dict
 
 
 if __name__ == "__main__":
